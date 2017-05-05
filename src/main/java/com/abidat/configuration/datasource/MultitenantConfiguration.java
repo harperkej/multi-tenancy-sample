667a0117b030c1b11e6a4c1bc868d449d87299cf
Arbër Kuçi <a.kuci@abidat.de>
2017-05-05 17:00:16 +0200

    Refactored configurations regarding the datasources

--- src/main/java/com/abidat/configuration/datasource/MultitenantConfiguration.java
+++ src/main/java/com/abidat/configuration/datasource/MultitenantConfiguration.java
@@ -1,83 +1,29 @@
 package com.abidat.configuration.datasource;
 
 
+import com.abidat.configuration.DataSourceConfigurations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
 
 @Configuration
 public class MultitenantConfiguration {
 
 
-    //TODO The names of configuration files maybe store in db, or any other better solution?
-    private List<String> tenantsConfigurationFiles = Arrays.asList("client_1.properties", "client_2.properties");
-
-    private String defaultTenant = "application.properties";
+    @Autowired
+    DataSourceConfigurations dataSourceConfigurations;
 
     @Bean
-    public DataSource createDataSource() {
-
-        Map<Object, Object> discoveredDataSources = new HashMap<>();
-
-        DriverManagerDataSource defaultDataSource = new DriverManagerDataSource();
-
-        //Get defualt configurations
-        try {
-            InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(defaultTenant);
-
-
-            Properties propertiesOfConfigFile = new Properties();
-            propertiesOfConfigFile.load(in);
-
-            String tenantIdentifier = propertiesOfConfigFile.getProperty("name");
-
-            defaultDataSource.setDriverClassName(propertiesOfConfigFile.getProperty("spring.datasource.driver-class-name"));
-            defaultDataSource.setUrl(propertiesOfConfigFile.getProperty("spring.datasource.url"));
-            defaultDataSource.setUsername(propertiesOfConfigFile.getProperty("spring.datasource.username"));
-            defaultDataSource.setPassword(propertiesOfConfigFile.getProperty("spring.datasource.password"));
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //Get custom configurations!
-        for (String tenant : tenantsConfigurationFiles) {
-            try {
-                InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(tenant);
-
-
-                Properties propertiesOfConfigFile = new Properties();
-                propertiesOfConfigFile.load(in);
-
-                String tenantIdentifier = propertiesOfConfigFile.getProperty("name");
-
-                DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-                dataSource.setDriverClassName(propertiesOfConfigFile.getProperty("spring.datasource.driver-class-name"));
-                dataSource.setUrl(propertiesOfConfigFile.getProperty("spring.datasource.url"));
-                dataSource.setUsername(propertiesOfConfigFile.getProperty("spring.datasource.username"));
-                dataSource.setPassword(propertiesOfConfigFile.getProperty("spring.datasource.password"));
-
-                discoveredDataSources.put(tenantIdentifier, dataSource);
-
-            } catch (IOException e) {
-
-                System.out.print(e);
-                e.printStackTrace();
-
-            }
-        }
-
+    public DataSource createDataSource() throws Exception {
         MultitenantDataSource multitenantDataSource = new MultitenantDataSource();
-        multitenantDataSource.setDefaultTargetDataSource(defaultDataSource);
-        multitenantDataSource.setTargetDataSources(discoveredDataSources);
-
+        try {
+            multitenantDataSource.setDefaultTargetDataSource(dataSourceConfigurations.getDefaultDataSource());
+            multitenantDataSource.setTargetDataSources(dataSourceConfigurations.getCustomDataSources());
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
         return multitenantDataSource;
     }
 
