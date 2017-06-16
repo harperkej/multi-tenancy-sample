5578508a11f9b2e5b8c68a3ff6793c9d1921f637
Arbër Kuçi <a.kuci@abidat.de>
2017-06-16 11:59:40 +0200

    Fixed an bug at the startup, the user.home directory was not takan correct -> for linux environment

--- src/main/java/com/abidat/example/schema/DatabaseSchemaUtilities.java
+++ src/main/java/com/abidat/example/schema/DatabaseSchemaUtilities.java
@@ -2,7 +2,8 @@ package com.abidat.example.schema;
 
 import com.abidat.example.configuration.common.Constant;
 
-import java.io.*;
+import java.io.File;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -25,10 +26,10 @@ final public class DatabaseSchemaUtilities {
      */
     public static void createSchema(String schemaName) throws Exception {
 
-        schemaName = schemaName.replace(" ", "");
+        schemaName = schemaName.replace( " ", "" );
 
         //Get the operating system in which the app is deployed!
-        String operatingSystem = System.getProperty("os.name");
+        String operatingSystem = System.getProperty( "os.name" );
 
         String batchOrShellCommand;
 
@@ -37,46 +38,46 @@ final public class DatabaseSchemaUtilities {
 
         Process process;
 
-        if (operatingSystem.startsWith(Constant.WINDOWS_OS)) {
+        if (operatingSystem.startsWith( Constant.WINDOWS_OS )) {
 
             //Get batch file from create_schema.bat
-            InputStream batchFileInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("create_schema.bat");
+            InputStream batchFileInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream( "create_schema.bat" );
             //If it is not the first time that app is being deployed -> delete the batch file from user.home directory!
-            Files.deleteIfExists(Paths.get(System.getProperty("user.home") + "\\create_schema.bat"));
+            Files.deleteIfExists( Paths.get( System.getProperty( "user.home" ) + "\\create_schema.bat" ) );
 
             //Create the file create_schema.bat to user.home directory.
-            File copiedBatchFileFromJar = new File(System.getProperty("user.home") + "\\create_schema.bat");
+            File copiedBatchFileFromJar = new File( System.getProperty( "user.home" ) + "\\create_schema.bat" );
 
             //Copy batch file from jar to user.home directory so that it can be executed.
-            Files.copy(batchFileInputStream, copiedBatchFileFromJar.toPath());
+            Files.copy( batchFileInputStream, copiedBatchFileFromJar.toPath() );
 
             //build the command.
-            batchOrShellCommand = "cmd /c " + System.getProperty("user.home") + "\\create_schema.bat " + schemaName;
+            batchOrShellCommand = "cmd /c " + System.getProperty( "user.home" ) + "\\create_schema.bat " + schemaName;
 
             //The directory where batch file is copeied
-            directoryWhereBatchOrShellFileIs = new File(System.getProperty("user.home"));
+            directoryWhereBatchOrShellFileIs = new File( System.getProperty( "user.home" ) );
             //Execute the batch file -> create schema for the tenant with companyNumber -> 'schemaName' -> without spaces!
-            process = Runtime.getRuntime().exec(batchOrShellCommand, null, directoryWhereBatchOrShellFileIs);
+            process = Runtime.getRuntime().exec( batchOrShellCommand, null, directoryWhereBatchOrShellFileIs );
         } else {
 
             //Get shell file create_schema.
-            InputStream shellFileputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("create_schema.sh");
+            InputStream shellFileputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream( "create_schema.sh" );
             //If it is ot the first time that app if being deployed, then shell file will be deleted from user.home directory
-            Files.deleteIfExists(Paths.get(System.getProperty("user.home") + "/create_schema.sh"));
+            Files.deleteIfExists( Paths.get( System.getProperty( "user.home" ) + "/create_schema.sh" ) );
             //
-            File copiedShellFileFromJar = new File(System.getProperty("user.home") + "/create_schema.sh");
+            File copiedShellFileFromJar = new File( System.getProperty( "user.home" ) + "/create_schema.sh" );
 
-            Files.copy(shellFileputStream, copiedShellFileFromJar.toPath());
+            Files.copy( shellFileputStream, copiedShellFileFromJar.toPath() );
 
-            batchOrShellCommand = "bash " + System.getProperty("user.home") + "/create_schema.sh " + schemaName;
+            batchOrShellCommand = "bash " + System.getProperty( "user.home" ) + "/create_schema.sh " + schemaName;
 
-            directoryWhereBatchOrShellFileIs = new File(System.getProperty("user.home"));
+            directoryWhereBatchOrShellFileIs = new File( System.getProperty( "user.home" ) );
 
             //give permission to execute the created shell file!
-            process = Runtime.getRuntime().exec("chmod u+x create_schema.sh", null, directoryWhereBatchOrShellFileIs);
+            process = Runtime.getRuntime().exec( "chmod u+x create_schema.sh", null, directoryWhereBatchOrShellFileIs );
 
             //execute shell file.
-            process = Runtime.getRuntime().exec(batchOrShellCommand, null, directoryWhereBatchOrShellFileIs);
+            process = Runtime.getRuntime().exec( batchOrShellCommand, null, directoryWhereBatchOrShellFileIs );
         }
     }
 
@@ -91,7 +92,7 @@ final public class DatabaseSchemaUtilities {
     public static void createDefaultSchemaAtStartup() throws Exception {
 
         //Detect the operating system!
-        String operatingSystem = System.getProperty("os.name");
+        String operatingSystem = System.getProperty( "os.name" );
 
         String batchOrShellCommand;
 
@@ -99,40 +100,40 @@ final public class DatabaseSchemaUtilities {
 
         Process p;
 
-        if (operatingSystem.startsWith(Constant.WINDOWS_OS)) {
+        if (operatingSystem.startsWith( Constant.WINDOWS_OS )) {
 
             //Get batch file from jar that creates the default db.
-            InputStream batchFileInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("create_default_schema.bat");
+            InputStream batchFileInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream( "create_default_schema.bat" );
             //Delete if there's any file in user.home directory with the name 'create_default_schema.bat'
-            Files.deleteIfExists(Paths.get(System.getProperty("user.home") + "\\create_default_schema.bat"));
+            Files.deleteIfExists( Paths.get( System.getProperty( "user.home" ) + "\\create_default_schema.bat" ) );
             //Create batch file in user.home directory that will create default db
-            File fileToCopyTo = new File(System.getProperty("user.home") + "\\create_default_schema.bat");
+            File fileToCopyTo = new File( System.getProperty( "user.home" ) + "\\create_default_schema.bat" );
             //copy the batch file for creating default db to user.home directory from jar -> user.home directory
-            Files.copy(batchFileInputStream, fileToCopyTo.toPath());
+            Files.copy( batchFileInputStream, fileToCopyTo.toPath() );
             //the command that will execute the batch in user.home directory for creating default db
-            batchOrShellCommand = "cmd /c " + System.getProperty("user.home") + "\\create_default_schema.bat";
+            batchOrShellCommand = "cmd /c " + System.getProperty( "user.home" ) + "\\create_default_schema.bat";
 
-            directoryWhereCopiedBatchOrShellFileIs = new File(System.getProperty("user.home"));
+            directoryWhereCopiedBatchOrShellFileIs = new File( System.getProperty( "user.home" ) );
 
-            p = Runtime.getRuntime().exec(batchOrShellCommand, null, directoryWhereCopiedBatchOrShellFileIs);
+            p = Runtime.getRuntime().exec( batchOrShellCommand, null, directoryWhereCopiedBatchOrShellFileIs );
         } else {
 
-            InputStream shellFileInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("create_default_schema.sh");
+            InputStream shellFileInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream( "create_default_schema.sh" );
 
-            Files.deleteIfExists(Paths.get(System.getProperty("user.home") + "/create_default_schema.sh"));
+            Files.deleteIfExists( Paths.get( System.getProperty( "user.home" ) + "/create_default_schema.sh" ) );
 
-            File fileToCopyTo = new File(System.getProperty("user.home") + "/create_default_schema.sh");
+            File fileToCopyTo = new File( System.getProperty( "user.home" ) + "/create_default_schema.sh" );
 
-            Files.copy(shellFileInputStream, fileToCopyTo.toPath());
+            Files.copy( shellFileInputStream, fileToCopyTo.toPath() );
 
-            batchOrShellCommand = "bash -c " + System.getProperty("user.home") + "/create_default_schema.sh ";
+            batchOrShellCommand = "bash -c " + System.getProperty( "user.home" ) + "/create_default_schema.sh ";
 
-            directoryWhereCopiedBatchOrShellFileIs = new File(System.getenv("user.home"));
+            directoryWhereCopiedBatchOrShellFileIs = new File( System.getProperty( "user.home" ) );
 
             //give permission to execute the created shell file!
-            p = Runtime.getRuntime().exec("chmod u+x create_default_schema.sh", null, directoryWhereCopiedBatchOrShellFileIs);
+            p = Runtime.getRuntime().exec( "chmod u+x create_default_schema.sh", null, directoryWhereCopiedBatchOrShellFileIs );
 
-            p = Runtime.getRuntime().exec(batchOrShellCommand, null, directoryWhereCopiedBatchOrShellFileIs);
+            p = Runtime.getRuntime().exec( batchOrShellCommand, null, directoryWhereCopiedBatchOrShellFileIs );
         }
         int res = p.waitFor();
     }
